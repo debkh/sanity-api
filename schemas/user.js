e9db070ae576a8b1f0d110f944c7e56de36b2d21
Dmitry Oplachko <dmitry@maxwell.ai>
2023-02-08 16:19:56 +0200

    Init commit

---
+++ schemas/user.js
@@ -0,0 +1,22 @@
+export default  {
+  name: 'user',
+  type: 'document',
+  title: 'User',
+  fields: [
+    {
+      name: 'first_name',
+      type: 'string',
+      title: 'First Name'
+    },
+    {
+      name: 'last_name',
+      type: 'string',
+      title: 'Last Name'
+    },
+    {
+      name: 'position',
+      type: 'string',
+      title: 'Position'
+    }
+  ]
+}
